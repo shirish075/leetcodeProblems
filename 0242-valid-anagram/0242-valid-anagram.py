f80b7e2b6f9caab95895f623cb193c258b611f24
shirish075 <113491003+shirish075@users.noreply.github.com>
2024-10-25 23:22:50 +0530

    Time: 18 ms (92.88%), Space: 17.7 MB (10.55%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -4,4 +4,8 @@ class Solution:
         b=list(i for i in t)
         a.sort()
         b.sort()
+        if a==b:
+            return True
+        else:
+            return False
         return a==b
